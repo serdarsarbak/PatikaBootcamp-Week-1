9d3615a896596c57a82886489564e29f37d4a366
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-12 08:44:43 -0500

    Git Hazırlık Projesi

--- Git-Hazirlik-Projesi/src/Main.java
+++ Git-Hazirlik-Projesi/src/Main.java
@@ -3,12 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int m = -12;
-        int n = -5;
-        int k = m%n;
+        System.out.print("Patika.dev FullStack Java Spring boot & React Bootcamp");
 
 
-        System.out.print(k);
+
 
 
     }
