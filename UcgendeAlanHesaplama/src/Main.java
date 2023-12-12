44aa45131187f599e826e832862905762d2ed3c0
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-12 08:16:18 -0500

    Ucgende Alan Hesaplama

---
+++ UcgendeAlanHesaplama/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+        double a, b, c, u, alan;
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1. Kenarı Giriniz: ");
+        a = input.nextInt();
+
+        System.out.print("2. Kenarı Giriniz: ");
+        b = input.nextInt();
+
+        System.out.print("3. Kenarı Giriniz: ");
+        c = input.nextInt();
+
+        u = (a+b+c) /2;
+
+        alan = Math.sqrt (u * (u-a)* (u-b) * (u-c));
+
+        System.out.print("Üçgenin alanı: " + alan);
+
+    }
+}
