69766b02fb15bc63c2a7dc0a090944495c96e903
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-13 17:05:07 -0500

    Aritmetik işlemler ve işlem önceliği

---
+++ AritmatikIslemler/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double a, b, c, total;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("a sayısını giriniz: ");
+        a = input.nextDouble();
+
+        System.out.print("b sayısını giriniz: ");
+        b = input.nextDouble();
+
+        System.out.print("c sayısını giriniz: ");
+        c = input.nextDouble();
+
+        System.out.println("Yapılacak işlem: a+b*c-b ");
+
+        total = a+b*c-b;
+
+        System.out.println("Sonuç: " + total);
+
+
+
+
+    }
+}
