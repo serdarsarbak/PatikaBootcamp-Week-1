e767f8d209d4ffa0cf89773ed1f6409647d1d65a
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-17 01:38:53 -0500

    Üslü sayı hesaplama

---
+++ UsluSayiHesaplama/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n, k, total=1;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Üssü alınacak sayı: ");
+        n = input.nextInt();
+
+        System.out.print("Üs olacak sayı: ");
+        k = input.nextInt();
+
+        for (int i=1; i<=k; i++) {
+            total *=n;
+        }
+
+        System.out.println("Sonuç: " + total);
+    }
+}
