5e6532a3e82a0856d97ce792645afd09ad22c6c2
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-17 00:07:12 -0500

    Sınır sayısına kadar kuvvet bulma

--- KuvvetleriniBulma/src/Main.java
+++ KuvvetleriniBulma/src/Main.java
@@ -12,13 +12,13 @@ public class Main {
         n = input.nextInt();
 
         System.out.println("4'ün kuvvetleri: ");
-        for (int i=1; i<n; i=i*4) {
+        for (int i=1; i<=n; i=i*4) {
             System.out.print(i + ", ");
         }
 
         System.out.println();
         System.out.println("5'ün kuvvetleri: ");
-        for (int i=1; i<n; i=i*5) {
+        for (int i=1; i<=n; i=i*5) {
             System.out.print(i + ", ");
         }
     }
