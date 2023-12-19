110c8f3f449091ba7cf7576ca8dd082ac727cdfa
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-19 18:26:09 -0500

    Tip dönüşümü

--- TipDonusumuCasting/src/Main.java
+++ TipDonusumuCasting/src/Main.java
@@ -3,13 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+        //Ondalık sayının girilmesi
         System.out.print("Lütfen ondalık bir sayı giriniz: ");
         double myDouble = input.nextDouble();
-        int doubleInt = (int) myDouble;
+        int doubleInt = (int) myDouble; // double'ın integara dönüşümü explicit casting
 
+        //Tam sayının girilmesi:
         System.out.print("Lütfen tam bir sayı giriniz: ");
         int myInt = input.nextInt();
-        double intDouble = myInt;
+        double intDouble = myInt; //integarın double'a dönüşümü otamatik implicit casting
+
+        //Sonuçların yazıdırılması:
 
         System.out.println(myDouble + " ondalık sayısının tam sayı hali: " + doubleInt );
         System.out.println(myInt + " tam sayısının ondalık hali: " + intDouble);
