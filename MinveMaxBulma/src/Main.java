ff11b4192dfe8685d44eabb3d810f29d3e8647cc
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-18 23:58:40 -0500

    Min ve max değer bulma

---
+++ MinveMaxBulma/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Java ile klavyeden girilen N tane sayma sayısından en büyük ve en küçük sayıları bulan ve
+        // bu sayıları ekrana yazan program.
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int num = input.nextInt();
+
+        // girilen ilk sayıyı hem en büyük hem de en küçük sayı olarak atanması:
+        System.out.print("1. sayıyı giriniz: ");
+        int lowestNum = input.nextInt();
+        int biggestNum = lowestNum;
+
+        //diğer girilen sayıların en küçük ve en büyük sayılar ile karşılaştırılması:
+        for (int i=2; i<=num; i++) {
+            System.out.print(i + ". sayıyı giriniz: ");
+            int a = input.nextInt();
+            if (a>biggestNum) {
+                biggestNum = a;
+            }
+            if (a<lowestNum) {
+                lowestNum = a;
+            }
+        }
+
+        // sonucun yazdırılması:
+        System.out.println("En küçük sayı: " + lowestNum);
+        System.out.println("En büyük sayı: " + biggestNum);
+
+
+
+    }
+}
